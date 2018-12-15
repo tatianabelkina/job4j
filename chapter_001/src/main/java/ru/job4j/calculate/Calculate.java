5529b151b93b22b61a679390183e51df8e849985
tatianabelkina <stooliv@gmail.com>
2018-12-15 21:41:29 +0300

    1.5. Checkstyle[#89190]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,8 +1,8 @@
 package ru.job4j.calculate;
 /**
-* Class Calculate решение задачи части 001 урок 1
-* @author Tatiana Belkina (stooliv@gmail.com)
-* @since 11.12.2018
+* Class Calculate решение задачи части 001 урок 1.
+* @author Tatiana Belkina (stooliv@gmail.com).
+* @since 11.12.2018.
 */
 
 public class Calculate {
@@ -10,7 +10,7 @@ public class Calculate {
 * Main.
 *@param args - args.
 */
-public static void main(String[] args){
+public static void main(String[] args) {
 
 System.out.println("Hello World");
 
