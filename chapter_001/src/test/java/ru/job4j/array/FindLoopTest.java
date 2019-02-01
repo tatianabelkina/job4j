e884cf23fd67c096890422b96fddb40b5bd65746
tatianabelkina <stooliv@gmail.com>
2019-02-01 18:01:27 +0300

    6.1. Классический поиск перебором. Тест # 2.

--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -16,4 +16,14 @@ public class FindLoopTest {
         int expect = 0;
         assertThat(result, is(expect));
     }
+
+    @Test
+    public void whenArrayHasLengh2ThenMinusOne() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[]{5, 10, 3};
+        int value = 2;
+        int result = find.indexOf(input, value);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
 }
