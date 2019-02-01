ebec0fc65b14a6f20b4a85c7b0748f368d5b91f9
tatianabelkina <stooliv@gmail.com>
2019-02-01 14:42:05 +0300

    6.0. Заполнить массив степенями чисел.

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,10 +1,12 @@
 package ru.job4j.loop;
 
 /**
+ *
  * @author Tatiana Belkina(stooliv@gmail.com).
  * @version $Id$
  * @since 29/01/2019
  */
+
 public class Paint {
     /**
      * Рисует правую часть пирамиды
@@ -16,11 +18,10 @@ public class Paint {
         // Буфер для результата.
         StringBuilder screen = new StringBuilder();
         // ширина будет равна высоте.
-        int weight = height;
         // внешний цикл двигается по строкам.
         for (int row = 0; row != height; row++) {
             // внутренний цикл определяет положение ячейки в строке.
-            for (int column = 0; column != weight; column++) {
+            for (int column = 0; column != height; column++) {
                 // если строка равна ячейки, то рисуем галку.
                 // в данном случае строка определяем, сколько галок будет на строке
                 if (row >= column) {
@@ -45,10 +46,9 @@ public class Paint {
      */
     public String leftTrl(int height) {
         StringBuilder screen = new StringBuilder();
-        int weight = height;
         for (int row = 0; row != height; row++) {
-            for (int column = 0; column != weight; column++) {
-                if (row >= weight - column - 1) {
+            for (int column = 0; column != height; column++) {
+                if (row >= height - column - 1) {
                     screen.append("^");
                 } else {
                     screen.append(" ");
