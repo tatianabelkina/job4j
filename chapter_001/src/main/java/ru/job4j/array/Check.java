b28a235f4db9f408f98502f88600b3f4766abcfe
tatianabelkina <stooliv@gmail.com>
2019-02-05 15:27:59 +0300

    6.3. Массив заполнен true или false.

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -11,21 +11,17 @@ public class Check {
      * Метод проверяет, что все элементы в массиве являются true или false.
      *
      * @param data .
-     * @return - true or false.
+     * @return boolean - true - все значения одинаковы, false - не все одинаковы
      */
 
     public boolean mono(boolean[] data) {
-        // цикл по элементам массива data.
-        // индекс i последнего элемента массива меньше length на 1.
-        // Поэтому length -1.
+        boolean result = false;
         for (int i = 0; i < data.length - 1; i++) {
-            // цикл сравнивает два соседних элемента массива.
-            if (data[i] != data[i + 1]) {
-                return false;
+            if (data[i] == data[i + 1]) {
+                result = true;
+                break;
             }
         }
-        return true;
+        return result;
     }
-
-
 }
